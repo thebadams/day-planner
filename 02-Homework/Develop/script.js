647b0c34df8d5a30db70e28d1b7a20d193111045
Brian Adams <bpeteradams@gmail.com>
2021-03-09 14:39:29 -0500

    wrote .each() to check datasets and match with past, present, future hour values

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -7,3 +7,22 @@ var currentDayValue = moment().format("dddd, MMMM Do");
 
 //set text value to currentDayValue
 currentDay.text(currentDayValue);
+
+//current hour logic using moment.js
+var currentHour = moment().hour();
+
+//check if current hour is between two times
+
+var timeBlocks = $(".time-block");
+
+timeBlocks.each(function(index){
+    if(this.dataset.hour == currentHour){
+        $(this).addClass("present");
+    } else if(this.dataset.hour < currentHour){
+        $(this).addClass("past");
+    } else if(this.dataset.hour > currentHour){
+        $(this).addClass("future");
+    }
+}
+)
+
